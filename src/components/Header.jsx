d6ed123b9d88ea2c27f1de300834de7678d696d4
angelo-lepore <angelo.lepore94@gmail.com>
2025-07-14 17:00:19 +0200

    Updated the title and added the navbar in the Header component; added content to the Main component; updated the CSS style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,21 @@ import Logo from './Logo'
 export default function Header(){
     return (
         <header>
-            <Logo/>
+            <div className="navbar">
+                <Logo/>
+                <ul>
+                    <li><a>CHARACTERS</a></li>
+                    <li><a>COMICS</a></li>
+                    <li><a>MOVIES</a></li>
+                    <li><a>TV</a></li>
+                    <li><a>GAMES</a></li>
+                    <li><a>COLLECTIBLES</a></li>
+                    <li><a>VIDEOS</a></li>
+                    <li><a>FANS</a></li>
+                    <li><a>NEWS</a></li>
+                    <li><a>SHOP</a></li>
+                </ul>
+            </div>
         </header>
     )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,33 @@
 export default function Main(){
     return (
         <main>
-            <p>Pippo</p>
+            <div className="container">
+                <p className="text">-- Content goes here --</p>
+            </div>
+            <div>
+                <div className="market-container">
+                    <div className="market">
+                        <img src='/img/buy-comics-digital-comics.png' alt="DC logo"/>
+                        <h5>DIGITAL COMICS</h5>
+                    </div>
+                    <div className="market">
+                        <img src='/img/buy-comics-merchandise.png' alt="DC logo"/>
+                        <h5>DC MERCHANDISE</h5>
+                    </div>
+                    <div className="market">
+                        <img src='/img/buy-comics-subscriptions.png' alt="DC logo"/>
+                        <h5>SUBSCRIPTION</h5>
+                    </div>
+                    <div className="market">
+                        <img src='/img/buy-comics-shop-locator.png' alt="DC logo"/>
+                        <h5>COMIC SHOP LOCATOR</h5>
+                    </div>
+                    <div className="market">
+                        <img src='/img/buy-dc-power-visa.svg' alt="DC logo"/>
+                        <h5>DC POWER VISA</h5>
+                    </div>
+                </div>
+            </div>
         </main>
     )
 }

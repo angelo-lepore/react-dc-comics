a84df8b0157eaebfc1706ad788b438f8f8979a09
angelo-lepore <angelo.lepore94@gmail.com>
2025-07-14 19:15:00 +0200

    The footer has been updated with new sections and links; the main page has been restructured to improve the presentation of the markets; and the CSS styling for the footer and navbar has been updated.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,71 @@
 export default function Footer(){
     return (
         <footer>
-            <p>Topolino</p>
+            <div className="container-shop">
+                <div>
+                    <div>
+                        <h4>DC COMICS</h4>
+                        <ul>
+                            <li><a>Characters</a></li>
+                            <li><a>Comics</a></li>
+                            <li><a>Movies</a></li>
+                            <li><a>TV</a></li>
+                            <li><a>Games</a></li>
+                            <li><a>Videos</a></li>
+                            <li><a>News</a></li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h4>SHOP</h4>
+                        <ul>
+                            <li><a>Shop DC</a></li>
+                            <li><a>Shop DC Collectibles</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div>
+                    <h4>DC</h4>
+                    <ul>
+                        <li><a>Terms Of Use</a></li>
+                        <li><a>Privacy policy (New)</a></li>
+                        <li><a>Ad Choices</a></li>
+                        <li><a>Advertising</a></li>
+                        <li><a>Jobs</a></li>
+                        <li><a>Subscriptions</a></li>
+                        <li><a>Talent Workshops</a></li>
+                        <li><a>CPSC Certificates</a></li>
+                        <li><a>Ratings</a></li>
+                        <li><a>Shop Help</a></li>
+                        <li><a>Contact Us</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h4>SITES</h4>
+                    <ul>
+                        <li><a>DC</a></li>
+                        <li><a>MAD Magazine</a></li>
+                        <li><a>DC Kids</a></li>
+                        <li><a>DC Universe</a></li>
+                        <li><a>DC Power Visa</a></li>
+                    </ul>
+                </div>
+                <div className="container-logo">
+                    <img src="/img/dc-logo-bg.png"/>
+                </div>
+            </div>
+            <div className="container-social">
+                <div>
+                    <button className="signup-btn">SIGN-UP NOW!</button>
+                </div>
+                <div className="social">
+                    <span>FOLLOW US</span>
+                    <img src="/img/footer-facebook.png" alt="Facebook" />
+                    <img src="/img/footer-twitter.png" alt="Twitter" />
+                    <img src="/img/footer-youtube.png" alt="YouTube" />
+                    <img src="/img/footer-pinterest.png" alt="Pinterest" />
+                    <img src="/img/footer-periscope.png" alt="Periscope" />
+                </div>
+            </div>
         </footer>
     )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,28 +4,26 @@ export default function Main(){
             <div className="container">
                 <p className="text">-- Content goes here --</p>
             </div>
-            <div>
-                <div className="market-container">
-                    <div className="market">
-                        <img src='/img/buy-comics-digital-comics.png' alt="DC logo"/>
-                        <h5>DIGITAL COMICS</h5>
-                    </div>
-                    <div className="market">
-                        <img src='/img/buy-comics-merchandise.png' alt="DC logo"/>
-                        <h5>DC MERCHANDISE</h5>
-                    </div>
-                    <div className="market">
-                        <img src='/img/buy-comics-subscriptions.png' alt="DC logo"/>
-                        <h5>SUBSCRIPTION</h5>
-                    </div>
-                    <div className="market">
-                        <img src='/img/buy-comics-shop-locator.png' alt="DC logo"/>
-                        <h5>COMIC SHOP LOCATOR</h5>
-                    </div>
-                    <div className="market">
-                        <img src='/img/buy-dc-power-visa.svg' alt="DC logo"/>
-                        <h5>DC POWER VISA</h5>
-                    </div>
+            <div className="market-container">
+                <div className="market">
+                    <img src='/img/buy-comics-digital-comics.png'/>
+                    <h5>DIGITAL COMICS</h5>
+                </div>
+                <div className="market">
+                    <img src='/img/buy-comics-merchandise.png'/>
+                    <h5>DC MERCHANDISE</h5>
+                </div>
+                <div className="market">
+                    <img src='/img/buy-comics-subscriptions.png'/>
+                    <h5>SUBSCRIPTION</h5>
+                </div>
+                <div className="market">
+                    <img style={{ width: '70px'}} src='/img/buy-comics-shop-locator.png'/>
+                    <h5>COMIC SHOP LOCATOR</h5>
+                </div>
+                <div className="market">
+                    <img src='/img/buy-dc-power-visa.svg'/>
+                    <h5>DC POWER VISA</h5>
                 </div>
             </div>
         </main>
